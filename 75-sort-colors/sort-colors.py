3fc068f874c82092493b98a9a1e515c14f47f1f1
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-04 20:26:12 +0530

    Time: 43 ms (18.46%) | Memory: 16.4 MB (92.91%) - LeetSync

--- 75-sort-colors/sort-colors.py
+++ 75-sort-colors/sort-colors.py
@@ -3,7 +3,26 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
+        # res=[nums[0]]
+        # curr=0
+        # for i in range(1,len(nums)):
+        #     if(nums[i]>res[curr]):
+        #         res.append(nums[i])
+        #         curr+=1
+        c0,c1,c2=0,0,0
+        # dict1={0:0,1:0,2:0}
         for i in range(len(nums)):
-            for j in range(len(nums)-i-1):
-                if(nums[j]>nums[j+1]):
-                    nums[j],nums[j+1]=nums[j+1],nums[j]
+            match nums[i]:
+                case 0:
+                    c0+=1
+                case 1:
+                    c1+=1
+                case 2:
+                    c2+=1
+        l0=[0]*c0
+        l1=[1]*c1
+        l2=[2]*c2
+        res=l0+l1+l2
+        for i in range(len(nums)):
+            nums[i]=res[i]                     
+            

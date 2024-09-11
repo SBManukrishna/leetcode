02f4718bcedfa9f86dffa200d54978cb8429cc05
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-11 17:39:01 +0530

    Time: 35 ms (86.21%) | Memory: 17.7 MB (43.32%) - LeetSync

--- 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
+++ 104-maximum-depth-of-binary-tree/maximum-depth-of-binary-tree.py
@@ -6,24 +6,51 @@
 #         self.right = right
 class Solution:
     def maxDepth(self, root: Optional[TreeNode]) -> int:
+        # if(root == None):
+        #     return 0
+
+        # def helper(root):
+        #     if root.left==None and root.right==None:
+        #         return 1
+
+        #     lDepth = 1
+        #     rDepth = 1
+
+        #     if(root.left!=None):
+        #         lDepth += helper(root.left)
+
+        #     if(root.right!=None):
+        #         rDepth += helper(root.right)
+
+        #     return max(lDepth, rDepth)
+                  
+        # return helper(root)  
+
         if(root == None):
             return 0
-        res=[]
+        global d,count
+        d=0
+        count=0
         def helper(root):
-            if root.left==None and root.right==None:
-                return 1
+            global count
+            count+=1
+            print(f"{root.val} , {count}\n")
 
-            lDepth = 1
-            rDepth = 1
+            if root.left==None and root.right==None:
+                global d
+                d=max(d, count)
+                count-=1
+                return
 
             if(root.left!=None):
-                lDepth += helper(root.left)
+                helper(root.left)
+
             if(root.right!=None):
-                rDepth += helper(root.right)
-            
-            return max(lDepth, rDepth)
-                  
-        return helper(root)               
+                helper(root.right)
+
+            count -= 1
+        helper(root)
+        return d           
         
 
         

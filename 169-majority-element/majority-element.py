8ac5cc20a676ec50fb62a2b62e32f620d29c2d38
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-20 11:48:18 +0530

    Time: 171 ms (55.14%) | Memory: 18.1 MB (21.34%) - LeetSync

--- 169-majority-element/majority-element.py
+++ 169-majority-element/majority-element.py
@@ -5,8 +5,9 @@ class Solution:
         for i in nums:
             if i in d:
                 d[i]+=1
+                if(d[i]>n/2):
+                    return i
             else:
                 d[i]=1
-        for j in d.keys():
-            if(d[j]>n/2):
-                return j
+        return i        
+    

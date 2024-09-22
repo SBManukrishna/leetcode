5f55d48ee8e1ed76e6f9dd516b214baee7687319
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-23 00:42:18 +0530

    Time: 43 ms (12.57%) | Memory: 16.6 MB (21.60%) - LeetSync

--- 66-plus-one/plus-one.py
+++ 66-plus-one/plus-one.py
@@ -1,4 +1,3 @@
 class Solution:
     def plusOne(self, digits: List[int]) -> List[int]:
-        num=int("".join(list(map(lambda x:str(x),digits))))+1
-        return list(map(lambda x:int(x),list(str(num))))
+        return list(map(lambda x:int(x),list(str(int("".join(list(map(lambda x:str(x),digits))))+1))))

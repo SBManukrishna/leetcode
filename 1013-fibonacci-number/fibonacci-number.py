6930090daf8ae98d4b24c621c8f3290fa9dd8b3a
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-25 21:15:21 +0530

    Time: 35 ms (68.59%) | Memory: 16.5 MB (11.15%) - LeetSync

--- 1013-fibonacci-number/fibonacci-number.py
+++ 1013-fibonacci-number/fibonacci-number.py
@@ -1,6 +1,5 @@
 class Solution:
-    @staticmethod
-    def fib(n: int) -> int:
+    def fib(self, n: int) -> int:
         f=[0,1]
         for i in range(2,n+1):
             f.append(f[i-1]+f[i-2])

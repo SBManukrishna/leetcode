070c29ec62e056bf6bde382331edd3864640cb33
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-09-29 12:42:35 +0530

    Time: 597 ms (9.27%) | Memory: 54.1 MB (7.26%) - LeetSync

--- 55-jump-game/jump-game.cpp
+++ 55-jump-game/jump-game.cpp
@@ -1,25 +1,30 @@
 class Solution {
-private:
-    int n;
-    bool help(vector<int>& nums , vector<int>& dp , int i) {
-        // base case
-        if(i == n-1)    return true;
-        if(i >= n)  return false;
-        // memoization case
-        if(dp[i] != -1) return dp[i];
-        // calling recursion
-        int maxJump = nums[i];
-        for(int jump = 1; jump <= maxJump; jump++) {
-            if(help(nums , dp , i + jump))
-                return dp[i] = 1;
-        }
-        return dp[i] = 0;
-    }
 public:
+    vector<int> dp;
+    int n;
+
+    bool helper(vector<int>& nums, int index) {
+        if(index == n - 1)
+            return true;
+        
+        if(nums[index] == 0)
+            return false;
+        
+        if(dp[index] != -1)
+            return dp[index];
+    
+        int end = nums[index];
+        for(int i = 1; i <= end; i++)
+            if(helper(nums, index + i))
+                return dp[index] = true;
+
+        return dp[index] = false;
+    }
+
     bool canJump(vector<int>& nums) {
-        // using recursion and memoization
         n = nums.size();
-        vector<int> dp(n , -1);
-        return help(nums , dp , 0);
+        dp.resize(n, -1);
+
+        return helper(nums, 0);
     }
 };

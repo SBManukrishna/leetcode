f5059dec0e7ca8073822e0d1223967fa0f08d4a9
SBManukrishna <102793379+SBManukrishna@users.noreply.github.com>
2024-10-01 18:40:33 +0530

    Time: 183 ms (63.97%) | Memory: 17.8 MB (21.21%) - LeetSync

--- 547-number-of-provinces/number-of-provinces.py
+++ 547-number-of-provinces/number-of-provinces.py
@@ -4,15 +4,15 @@ class Solution:
         res=0
 
         def dfs(root):
-            if(root + 1 not in visited):
-                visited.add(root + 1)
+            if(root not in visited):
+                visited.add(root)
                 for neighbour in range(len(isConnected)):
-                    if isConnected[root][neighbour] == 1 and neighbour + 1 not in visited:
+                    if isConnected[root][neighbour] == 1 and neighbour not in visited:
                         dfs(neighbour)
         
         for i in range(len(isConnected)):
             for j in range(len(isConnected)):
-                if i + 1 not in visited and isConnected[i][j] == 1:
+                if i not in visited and isConnected[i][j] == 1:
                     dfs(i)
                     res+=1
         return res
